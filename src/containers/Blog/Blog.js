d14974e5f40dcf17a786db31b65994d44a870951
Erdem Bozkurt <ebozkurt@sabanciuniv.edu>
2018-07-24 22:01:37 +0300

    video 158

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -10,37 +10,46 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Max'
-                }
-            });
-            this.setState({posts: updatedPosts})
-            // console.log(response);
-        })
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({ posts: updatedPosts })
+                // console.log(response);
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({ error: true });
+            })
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.setState({ selectedPostId: id });
 
 
     }
 
-    render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)} />
-        });
+    render() {
+        let posts = <p style={{ textAlign: 'center' }} >Something went wrong</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
